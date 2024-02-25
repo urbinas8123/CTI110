4036d07edbc34e87e196b7b657dcffe294395b6f
urbinas8123 <160445456+urbinas8123@users.noreply.github.com>
2024-02-25 00:33:21 -0500

    integers and math

---
+++ P1HW1_UrbinaSteven.py
@@ -0,0 +1,27 @@
+#Steven Urbina
+
+#Feb 25 2024
+
+#p1HW1
+
+#Putting integers in and doing math with Python
+
+
+
+
+
+
+
+
+integer = int(input("Enter integer:"))
+print("You Entered:", integer)
+square_integer = integer**2
+print(f'{integer} squared is {integer**2}')
+cube_integer = integer**3
+print(f'{integer} cubed is {integer**3} !!')
+integer2 = int(input('Enter another integer:'))
+sum_integers = integer + integer2
+print(f"{integer} + {integer2} is {sum_integers}")
+product_integers = integer * integer2
+print(f"{integer} * {integer2} is {product_integers}")
+      
